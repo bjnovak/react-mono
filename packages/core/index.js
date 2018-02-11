c8c903d9de92dae18adb6da1baba9d3483125e81
Bryan Novak <bjnovak125@gmail.com>
2018-02-10 20:47:01 -0600

    adding core package

---
+++ packages/core/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.css';
+
+export class CoreMod extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            btnColor: 'btn-blue'
+        };
+    }
+
+    clickMe() {
+        this.setState((prevState, props) => ({
+            btnColor: prevState == 'btn-blue' ? 'btn-red' : 'btn-blue'
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>React</h1>
+                <button onClick={this.clickMe} className={}>click me</button>
+            </div>
+        )
+    }
+}
