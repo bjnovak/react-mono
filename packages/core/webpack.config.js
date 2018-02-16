8d86b4fe0edb17434d15b127bfcd0c7ab24f15e4
Bryan Novak <bjnovak125@gmail.com>
2018-02-15 23:43:47 -0600

    adding webpack config for core package

---
+++ packages/core/webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    externals: {
+        react: 'React'
+    },
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['lib']),
+        new UglifyJSPlugin({
+            sourceMap: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react'],
+                        forceEnv: 'production'
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'lib')
+    }
+};
